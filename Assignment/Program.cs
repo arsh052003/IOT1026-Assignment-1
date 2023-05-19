a47975d7422330b6aeff0677ba7e56eecfbbead5
arsh052003 <122691385+arsh052003@users.noreply.github.com>
2023-05-19 22:47:17 +0000

    Documentation and Comments

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -8,8 +8,9 @@
         /// <param name="original">The array to be replicated</param>
         /// <returns>A deep copy of the original array</returns>
         public static int[] ReplicateArray(int[] original)
-        {
+        {   //setting replicated array length based on original array length
             int[] copyArray = new int[original.Length];
+            // using for loop to set the replicated array index value based on the original array index value
             for (int j = 0; j < original.Length; ++j)
             {
                 copyArray[j] = original[j];
@@ -26,33 +27,33 @@
         /// <returns>The user input as an integer</returns>
         public static int AskForNumber(string text)
         {
-
             string response = "";
             Console.WriteLine(text);
-            /* Checking for condition if the string is empty or not
-               While loop forces the user to enter the value as it doesnot exit until 
-               the value os not null.
+            /* 
+               Checking for condition if the string is empty or not
+               While loop forces the user to enter the value as it does not exit until 
+               the value is not null or string.
              */
             int resultnumber = 0;
             while (true)
             {
                 response = Console.ReadLine();
-                if (string.IsNullOrEmpty(response) || response.All(char.IsLetter)) // built in function for checking the empty string
+                /*
+                 checking is user input is null, empty or a string. 
+                 if any of these conditions meet, prompt user to renter value
+                 If not, return the String value as an integer
+                */
+                if (string.IsNullOrEmpty(response) || response.All(char.IsLetter))
                 {
                     Console.WriteLine("Input not valid. Please enter an integer value. ");
 
                 }
-
                 else
-
                 {
                     resultnumber = Int32.Parse(response); // Conversion of the string into int32
                     return resultnumber;
-
                 }
-
             }
-
         }
 
         /// <summary>
@@ -66,9 +67,12 @@
         public static int AskForNumberInRange(string text, int min, int max)
         {
             string textRange = string.Format("{0} [{1}, {2}]", text, min, max);
-            // Using the same function AskForNumber inside the class to avoid repeatibility of the code
+            // Using the function AskForNumber to get user input to avoid repeatibility of the code
             int rangeNum = ArrayReplicator.AskForNumber(textRange);
-            // re prompting the user if the value is not in the interval. Here part of task 1 is integrated with task 2.
+            /*
+                re prompting the user if the value is not in the interval range. 
+                Here part of task 1 is integrated with task 2.
+            */
             while (true)
             {
 
@@ -88,8 +92,6 @@
 
                 }
             }
-
-
         }
     }
 
@@ -122,8 +124,6 @@
             // Verify original and replicated array are the same
             for (int index = 0; index < size; ++index)
                 Console.WriteLine($"Original {original[index],-PrintOffset}  {copy[index],4} Copy");
-
-
 
         }
     }
